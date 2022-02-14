712d33b10dd7a7f48abaecf94122be8245109ea2
Cyril Li <76818434+CyrilSLi@users.noreply.github.com>
2022-02-14 14:02:18 -0600

    Add files via upload

---
+++ num.py
@@ -0,0 +1,10 @@
+exp = 2
+while True:
+    exp += 1
+    print ("Calculating", exp)
+    for i in range (10 ** (exp - 2), 10 ** (exp + 1)):
+        num = 0
+        for j in str (i):
+            num += int (j) ** exp
+        if num == i:
+            print (i, exp)
